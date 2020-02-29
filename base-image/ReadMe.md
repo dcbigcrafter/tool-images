a963773778d6ebde3e912a52ed96f3522bf2f14f
duchao <243648965@qq.com>
2020-02-29 19:43:00 +0800

    base images

---
+++ base-image/ReadMe.md
@@ -0,0 +1,11 @@
+制作其他镜像所用的基础镜像
+
+镜像内工具(预备)：
+curl
+telnet
+netstat
+ping
+vim
+bash
+top
+free
---
+++ base-image/centos-base/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+arch=""
+#根据不同平台制作不同镜像
+if [ `uname -m` = "x86_64" ];then
+	arch="amd64"
+elif [ `uname -m` = "aarch64" ];then
+	arch="arm64"
+else
+	echo "arch:`uname -m` not supported"
+	exit
+fi
+
+#制作镜像
+docker build -t centos-7-base:v0.0.1 . #\
+# --network=host
+# --build-arg http_proxy=http://172.16.0.4:8118\
+# --build-arg https_proxy=http://172.16.0.4:8118
